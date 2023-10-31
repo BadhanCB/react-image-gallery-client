77370548a132b125964b66623bcae77f420e06eb
BadhanCB <badhancsesastc112@gmail.com>
2023-11-01 02:41:03 +0600

    configure tailwindcss

---
+++ src/components/Gallery.tsx
@@ -0,0 +1,11 @@
+// type Props = {}
+
+const Gallery = () => {
+  return (
+    <main>
+        <h1 className="text-5xl text-center font-thin">Image Gallery Area</h1>
+    </main>
+  )
+}
+
+export default Gallery;
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+// type Props = {}
+
+const Header = () => {
+  return (
+    <header>
+        <h1 className="text-xl text-center font-thin">Header</h1>
+    </header>
+  )
+}
+
+export default Header;
